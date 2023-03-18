acf9e19d1afbed9a45ea9d325620fc97e38b4043
Zakia Aktar <zakiaaktar@Zakias-MacBook-Pro.local>
2023-03-18 20:44:50 +0100

    add available appointment section

---
+++ src/Pages/Appointment/AvailableAppointments/AppointmentOption.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const AppointmentOption = ({ appointmentOption}) =>{
+    const { name, price, slots } = appointmentOption;
+
+
+
+    return (
+         <div className="card shadow-xl w-96 mx-auto py-12 rounded-none hover:bg-gray-100">
+            <div className="card-body text-center">
+                <h2 className="text-2xl text-red-700 text-center font-bold">{name}</h2>
+                <p>{ slots.length > 0 ? slots[0] : 'Try Another day'}</p>
+                <p>{slots.length} {slots.length > 1 ? 'spaces' : 'space'}</p>
+                <p><small>Price: ${price}</small></p>
+                {/* <div className="card-actions justify-center">
+                   <label disabled ={slots.length === 0} onClick={() => setTreatment(appointmentOption)} htmlFor="booking-modal" className="btn btn-primary text-white">Book Appointment</label>
+                </div> */}
+            </div>
+        </div>
+    );
+};
+
+export default AppointmentOption;
--- src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -1,10 +1,35 @@
 import { format } from 'date-fns';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import AppointmentOption from './AppointmentOption';
 
-const AvailableAppointments = ({selectedDate}) => {
+const AvailableAppointments = ({ selectedDate }) => {
+    const [appointmentOptions, setAppointmentOptions] = useState([]);
+
+
+
+
+    useEffect(() => {
+        fetch('appointmentOptions.json')
+            .then(res => res.json())
+            .then(data => setAppointmentOptions(data))
+    }, [])
+
+
+
+
+
     return (
         <section className='my-16'>
-            <p className='text-center text-red-700 font-bold'>Available Appointments on {format(selectedDate, 'PP')}</p>
+            <p className='text-center text-xl text-red-700 font-bold'>Available Appointments on {format(selectedDate, 'PP')}</p>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16 mt-12 px-24'>
+                {
+                    appointmentOptions.map(option => <AppointmentOption
+                        key={option._id}
+                        appointmentOption={option}
+                    // setTreatment={setTreatment}
+                    ></AppointmentOption>)
+                }
+            </div>
         </section>
     );
 };
